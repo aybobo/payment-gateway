ec623a28c09891745f1dd081f84633e669b14033
Ayo <hayorfad@gmail.com>
2022-11-07 16:23:07 -0800

    add updates

---
+++ app/Models/Card.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name_on_card',
+        'card_number',
+        'cvc',
+        'expire_month',
+        'expire_year',
+    ];
+}
---
+++ database/migrations/2022_11_04_173337_create_cards_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('name_on_card');
+            $table->integer('card_number');
+            $table->integer('cvc');
+            $table->integer('expire_month');
+            $table->integer('expire_year');
+            $table->string('status')->default('No');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
